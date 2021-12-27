4e06aa57a9443a442011c7bb482abe91be91cb29
Arfan Salman Ramadhan <arfansalman9@gmail.com>
2021-12-27 13:46:29 +0700

    menambahkan komentar di file js.js

--- Kalkulator-vanilla/js/script.js
+++ Kalkulator-vanilla/js/script.js
@@ -1,16 +1,16 @@
-function bersaing(angka) {
-  document.getElementById("value").value = document.getElementById("value").value + angka;
+function bersaing(angka) { // membuat angka dan operator
+  document.getElementById("value").value = document.getElementById("value").value + angka; 
 }
-function hitung() {
+function hitung() { // menghitung angka
   let hitung = document.getElementById("value").value;
   if (hitung) {
     document.getElementById("value").value = eval(hitung);
   }
 }
-function jernih() {
+function jernih() {  // menghapus angka dan operator sekaligus
   document.getElementById("value").value = "";
 }
-function hapus() {
+function hapus() { // menghapus angka dan operator secara satu persatu
   let a = document.getElementById("value").value;
   document.getElementById("value").value = a.substring(0, a.length - 1);
 }
